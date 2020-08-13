2afdb046c456dcf8067122acf06aadca7b877bed
AsterinGray <raymondtiandinatan@gmail.com>
2020-08-13 18:27:16 +0700

    Homepage Refactor

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -69,11 +69,14 @@
                 @endif
             @endauth
           @endif -->
-            <li class="nav-item">
+            <!-- <li class="nav-item">
                 <a class="text-light nav-link" href="/ login">Login</a>
             </li>
             <li class="nav-item">
                 <a class="text-light nav-link" id="register" href="/register">Register</a>
+            </li> -->
+            <li class="nav-item">
+                <span class="text-light nav-link" id="register">Comming Soon</span>
             </li>
           </ul>
         </div>
